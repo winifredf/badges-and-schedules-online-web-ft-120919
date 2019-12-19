dd0bc2786f5fd3c3039ed101af495a03382fcbf8
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 23:52:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(speakers)
   speakers.map.with_index do |name, index|
-    attendees.each_with_index{|name, index| greeting << "Hello, #{name!} You'll be assigned to room #{index+1}!"}
+    "Hello, #{name!} You'll be assigned to room #{index+1}!"}
   end
   
 greeting
