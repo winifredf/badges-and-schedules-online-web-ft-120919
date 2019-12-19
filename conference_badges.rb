fa4029b411ef7e51294f74978810bdddb452fefc
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 23:52:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,8 @@ def batch_badge_creator(name)
 end
 
 def assign_rooms(speakers)
-  speakers.map.with_index do  = []
-  attendees.each_with_index{|name, index| greeting << "Hello, #{name!} You'll be assigned to room #{index+1}!"}
+  speakers.map.with_index do |name, index|
+    attendees.each_with_index{|name, index| greeting << "Hello, #{name!} You'll be assigned to room #{index+1}!"}
   end
   
 greeting
